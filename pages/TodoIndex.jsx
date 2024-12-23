13ecaab472c60c2089d143d660366a9b867e312a
nadavel <kamekazeboy85@gmail.com>
2024-12-23 08:32:11 +0200

    Todo reducer and actions added, index was refactored for Redux

--- pages/TodoIndex.jsx
+++ pages/TodoIndex.jsx
@@ -3,40 +3,45 @@ import { TodoList } from "../cmps/TodoList.jsx"
 import { DataTable } from "../cmps/data-table/DataTable.jsx"
 import { todoService } from "../services/todo.service.js"
 import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
+import { loadTodos, removeTodo, removeTodoOptimistic, saveTodo } from "../store/actions/todo.actions.js"
+import { SET_FILTER_BY } from '../store/reducers/todo.reducer.js'
 
 const { useState, useEffect } = React
-const { Link, useSearchParams } = ReactRouterDOM
+const { useSelector, useDispatch } = ReactRedux
+const { Link } = ReactRouterDOM
 
 export function TodoIndex() {
 
-    const [todos, setTodos] = useState(null)
-
-    // Special hook for accessing search-params:
-    const [searchParams, setSearchParams] = useSearchParams()
-
-    const defaultFilter = todoService.getFilterFromSearchParams(searchParams)
-
-    const [filterBy, setFilterBy] = useState(defaultFilter)
+    const todos = useSelector(storeState => storeState.todoModule.todos)
+    const isLoading = useSelector(storeState => storeState.todoModule.isLoading)
+    const filterBy = useSelector(storeState => storeState.todoModule.filterBy)
+    const dispatch = useDispatch()
 
     useEffect(() => {
-        setSearchParams(filterBy)
-        todoService.query(filterBy)
-            .then(todos => setTodos(todos))
-            .catch(err => {
-                console.eror('err:', err)
-                showErrorMsg('Cannot load todos')
-            })
+        loadTodos()
+            .catch(err => console.log('err:', err))
     }, [filterBy])
 
+    function onSetFilter(filterBy) {
+        dispatch({ type: SET_FILTER_BY, filterBy })
+    }
+
     function onRemoveTodo(todoId) {
-        todoService.remove(todoId)
-            .then(() => {
-                setTodos(prevTodos => prevTodos.filter(todo => todo._id !== todoId))
-                showSuccessMsg(`Todo removed`)
+        removeTodoOptimistic(todoId)
+            .then(() => showSuccessMsg('Car removed'))
+            .catch(err => showErrorMsg('Cannot remove car'))
+    }
+
+    function onEditTodo(todo) {
+        const importance = +prompt('New importance?', todo.importance)
+        const todoToSave = { ...todo, importance }
+
+        saveTodo(todoToSave)
+            .then((savedTodo) => {
+                showSuccessMsg(`Todo updated to importance: ${savedTodo.importance}`)
             })
-            .catch(err => {
-                console.log('err:', err)
-                showErrorMsg('Cannot remove todo ' + todoId)
+            .catch(() => {
+                showErrorMsg(`Cannot update todo`)
             })
     }
 
@@ -53,20 +58,25 @@ export function TodoIndex() {
             })
     }
 
-    if (!todos) return <div>Loading...</div>
     return (
-        <section className="todo-index">
-            <TodoFilter filterBy={filterBy} onSetFilterBy={setFilterBy} />
-            <div>
-                <Link to="/todo/edit" className="btn" >Add Todo</Link>
-            </div>
-            <h2>Todos List</h2>
-            <TodoList todos={todos} onRemoveTodo={onRemoveTodo} onToggleTodo={onToggleTodo} />
-            <hr />
-            <h2>Todos Table</h2>
-            <div style={{ width: '60%', margin: 'auto' }}>
-                <DataTable todos={todos} onRemoveTodo={onRemoveTodo} />
-            </div>
-        </section>
+        <div className='todo-index'>
+            <h3>Todos App</h3>
+            <main>
+                <section>
+                    <button className='add-btn'><Link to={`/todo/edit`}>Add Todo</Link></button>
+                </section>
+                <TodoFilter filterBy={filterBy} onSetFilter={onSetFilter} />
+                {!isLoading
+                    ? <TodoList
+                        todos={todos}
+                        onRemoveTodo={onRemoveTodo}
+                        onEditTodo={onEditTodo}
+                        onToggleTodo={onToggleTodo}
+                    />
+                    : <div>Loading..</div>
+                }
+                <hr />
+            </main>
+        </div>	
     )
 }
